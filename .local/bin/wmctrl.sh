b0906c8d0ac8605499c81672ab0a0f02f4be522c
Weiwu Zhang <a@colourful.land>
2019-12-29 11:07:26 +1100

    best combination found so far

--- .local/bin/wmctrl.sh
+++ .local/bin/wmctrl.sh
@@ -10,16 +10,16 @@ fi
 
 if [ "$resolution" = "2560x1440" ]
 then
-	wmctrl -x -r Navigator      -e 0,70,000,1200,1380
-	wmctrl -x -r Gnome-terminal -e 0,1267,000,0734,1370
+	wmctrl -x -r Firefox        -e 0,70,000,1200,1380
 	wmctrl -x -r Telegram       -e 0,1700,000,0861,700
 	wmctrl -x -r Signal         -e 0,1700,790,0860,650
+	wmctrl -x -r Gnome-terminal -e 0,1290,000,0734,1370
 fi
 
 if [ "$resolution" = "1920x1080" ]
 then
 	wmctrl -x -r Firefox        -e 0,70,000,1024,1015
-	wmctrl -x -r Telegram       -e 0,1094,000,0821,1100
-	wmctrl -x -r Signal         -e 0,1094,000,0821,1100
-	wmctrl -x -r Gnome-terminal -e 0,1068,000,0866,1100
+	wmctrl -x -r Telegram       -e 0,1094,000,0821,1000
+	wmctrl -x -r Signal         -e 0,1094,000,0821,1020
+	wmctrl -x -r Gnome-terminal -e 0,1068,000,0866,1025
 fi
