dc281be3c51356529cfaa711f7c55b15f5570e72
Weiwu Zhang <a@colourful.land>
2020-02-02 11:36:47 +1100

    if Firefox is maximized, undo it before adjusting its size

--- .local/bin/wmctrl.sh
+++ .local/bin/wmctrl.sh
@@ -10,6 +10,7 @@ if [ "$resolution" = "2560x1440" ]
 then
 	wmctrl -x -r Telegram       -e 0,1700,000,0861,700
 	wmctrl -x -r Signal         -e 0,1700,790,0860,650
+	wmctrl -x -r Firefox -b remove,maximized_vert,maximized_horz
 	wmctrl -x -r Firefox        -e 0,70,000,1245,1380
 	wmctrl -x -r Firefox -b add,maximized_vert
 	wmctrl -x -r Gnome-terminal -e 0,1290,000,0734,1370
@@ -21,8 +22,9 @@ if [ "$resolution" = "1920x1080" -o "$resolution" = "1920x1200" ]
 then
 	wmctrl -x -r Telegram       -e 0,1094,000,0821,1000
 	wmctrl -x -r Signal         -e 0,1094,000,0821,1020
-	wmctrl -x -r Firefox        -e 0,0070,000,1024,1015
 	wmctrl -x -r Gnome-terminal -e 0,1068,000,0866,1025
+	wmctrl -x -r Firefox -b remove,maximized_vert,maximized_horz
+	wmctrl -x -r Firefox        -e 0,0070,000,1024,1015
 	wmctrl -x -r Firefox -b add,maximized_vert
 	wmctrl -x -r Gnome-terminal -b add,maximized_vert
 	exit
