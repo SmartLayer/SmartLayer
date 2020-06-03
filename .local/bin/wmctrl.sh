d657bb6a67004c5b5f08bde4e5b5529711ded301
Weiwu Zhang <a@colourful.land>
2020-06-03 23:13:07 +1000

    as it turned out xrandr can't honestly report resolution, xdpyinfo can

--- .local/bin/wmctrl.sh
+++ .local/bin/wmctrl.sh
@@ -1,25 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 # HTML-1 is the external monitor when connected to the HTML port
 # DP-1 is the external monitor when connected to a display port through USB-C
 
-export resolution=`xrandr | awk 'external == 1 && $0  ~ /[+]/ {print $1; exit } $2=="connected" { if ($1 ~ "^DP-" || $1 ~ "^HDMI-") external = 1} '`
+#export resolution=`xrandr | awk 'external == 1 && $0  ~ /[+]/ {print $1; exit } $2=="connected" { if ($1 ~ "^DP-" || $1 ~ "^HDMI-" || $1 ~ "Virtual1") external = 1} '`
+export `xdpyinfo | grep dimension | awk  '{print $1 $2;}' | tr : =`
 
 # 3 column layout
-if [ "$resolution" = "2560x1440" ]
+if [[ ${dimensions} = 2560* ]]
 then
 	wmctrl -x -r Telegram       -e 0,1700,000,0861,700
 	wmctrl -x -r Signal         -e 0,1700,790,0860,650
 	wmctrl -x -r Firefox -b remove,maximized_vert,maximized_horz
-	wmctrl -x -r Firefox        -e 0,750,0,1245,1374
+	wmctrl -x -r Firefox        -e 0,805,0,1200,1374
 	# so that the height can still be mouse-adjusted
 	#wmctrl -x -r Firefox -b add,maximized_vert
-	wmctrl -x -r Gnome-terminal -e 0,45,000,0734,1370
+	wmctrl -x -r Gnome-terminal -e 0,45,000,0790,1370
 	exit
 fi
 
 # 2 column layout
-if [ "$resolution" = "1920x1080" -o "$resolution" = "1920x1200" ]
+if [[ ${dimensions} = 1920* ]]
 then
 	wmctrl -x -r Telegram       -e 0,1068,000,0821,1000
 	wmctrl -x -r Signal         -e 0,1068,000,0821,1020
@@ -31,10 +32,10 @@ then
 	exit
 fi
 
-if [ "$resolution" = "" ]
+if [ "$dimensions" = "" ]
 then
 	echo "no external monitor."
 else
-	echo "no configuration for resolution $resolution"
+	echo "no configuration for resolution $dimensions"
 fi
 
