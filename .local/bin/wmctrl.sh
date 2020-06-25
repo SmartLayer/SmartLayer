8e1240428c31770d846a96c28ccd302c2e60b348
Weiwu Zhang <a@colourful.land>
2020-06-25 14:54:07 +1000

    updated to allow 3440×1440 display

--- .local/bin/wmctrl.sh
+++ .local/bin/wmctrl.sh
@@ -6,19 +6,6 @@
 #export resolution=`xrandr | awk 'external == 1 && $0  ~ /[+]/ {print $1; exit } $2=="connected" { if ($1 ~ "^DP-" || $1 ~ "^HDMI-" || $1 ~ "Virtual1") external = 1} '`
 export `xdpyinfo | grep dimension | awk  '{print $1 $2;}' | tr : =`
 
-# 3 column layout
-if [[ ${dimensions} = 2560* ]]
-then
-	wmctrl -x -r Telegram       -e 0,1700,000,0861,700
-	wmctrl -x -r Signal         -e 0,1700,790,0860,650
-	wmctrl -x -r Firefox -b remove,maximized_vert,maximized_horz
-	wmctrl -x -r Firefox        -e 0,805,0,1200,1374
-	# so that the height can still be mouse-adjusted
-	#wmctrl -x -r Firefox -b add,maximized_vert
-	wmctrl -x -r Gnome-terminal -e 0,45,000,0790,1370
-	exit
-fi
-
 # 2 column layout
 if [[ ${dimensions} = 1920* ]]
 then
@@ -29,8 +16,35 @@ then
 	wmctrl -x -r Firefox        -e 0,70,000,1024,1015
 	wmctrl -x -r Firefox -b add,maximized_vert
 	wmctrl -x -r Gnome-terminal -b add,maximized_vert
-	exit
 fi
+
+# 3 column layout
+if [[ ${dimensions} = 2560* ]]
+then
+	wmctrl -x -r Telegram       -e 0,1700,000,0861,700
+	wmctrl -x -r Signal         -e 0,1700,790,0860,650
+	wmctrl -x -r Firefox -b remove,maximized_vert,maximized_horz
+	wmctrl -x -r Firefox        -e 0,805,0,1200,1374
+	# so that the height can still be mouse-adjusted
+	#wmctrl -x -r Firefox -b add,maximized_vert
+	wmctrl -x -r Gnome-terminal -e 0,45,000,0790,1370
+fi
+
+# 4 column layout
+if [[ ${dimensions} = 3440* ]]
+then
+	wmctrl -x -r Telegram       -e 0,2579,000,0861,700
+	wmctrl -x -r Signal         -e 0,2579,790,0860,650
+	wmctrl -x -r Firefox -b remove,maximized_vert,maximized_horz
+	wmctrl -x -r Firefox        -e 0,1378,0,1200,1374
+	# so that the height can still be mouse-adjusted
+	#wmctrl -x -r Firefox -b add,maximized_vert
+
+	# this would allow Gnome-terminal to be of 80 columns
+	#wmctrl -x -r Gnome-terminal -e 0,618,000,0790,1370
+	wmctrl -x -r Gnome-terminal -e 0,45,000,1363,1370
+fi
+
 
 if [ "$dimensions" = "" ]
 then
