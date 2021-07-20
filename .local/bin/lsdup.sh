a81df4f261ae0a3f162c17cf7b405f60f0de950a
Weiwu Zhang <a@colourful.land>
2021-07-20 11:40:11 +1000

    1. use dedup.sh to find dupes to remove 2. use the alternative dedup.sh to find dupes with same names, they are safe to remove 3. use lsdup.sh to compare the filenames, and you may choose the filenames to kee/remove

--- .local/bin/lsdup.sh
+++ .local/bin/lsdup.sh
@@ -7,10 +7,6 @@
 # example 
 # dedup.sh dir_to_keep/ MEGAsync\ Downloads/ | xargs -d $'\n' rm
 
-# dedup.sh -s
-
 du -ab "$1" | awk -F $"\t" '$1 > 104857600 {print;}' | sort > /tmp/keep
 du -ab "$2" | awk -F $"\t" '$1 > 104857600 {print;}' | sort > /tmp/dup
-#join -j 1 -t $'\t' /tmp/keep /tmp/dup | awk -F $"\t" '{fn1 = $2; fn2 = $3; sub(/.*\//, "", fn1);sub(/.*\//, "", fn2); if (fn1 == fn2) print $3;}' 
-join -j 1 -t $'\t' /tmp/keep /tmp/dup | awk -F $"\t" '{print $3;}' 
-
+join -j 1 -t $'\t' /tmp/keep /tmp/dup | column -t -s $'\t'
